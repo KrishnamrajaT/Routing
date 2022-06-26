048bb9f1eb807596da28ad4ff7899950e47d40c6
KrishnamrajaT <106855792+KrishnamrajaT@users.noreply.github.com>
2022-06-26 12:02:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+import react from "react";
+import nav from "./pages/nav";
+import Nav from "./pages/nav";
+import Home from "./pages/home";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Login from "./pages/login";
+import About from "./pages/about";
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/Login" element={<Login />}></Route>
+          <Route exact path="/About" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ src/pages/nav.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <div>
+      <nav>
+        <ul className="nav-links">
+          <Link to="/login">
+            <li>login</li>
+          </Link>
+          <Link to="/about">
+            <li>about</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
