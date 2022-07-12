43b05deaa202f4a8ee7468b11af5b6457ecd630d
Krishnamraja <rajkrishnam120@gmail.com>
2022-07-12 18:37:57 +0530

    add

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,11 @@ export default function Home() {
     <div>
       <button>Home page</button>
     <p>
-    Search Preschool Sydney, Top Information From Trusted Internet Sources. Preschool Sydney, Get Expert Advice and Curated Content on Top10Quest.com. 100+ Unique Results. Useful & Relevant. Find All Info You Want. Quick Answers. Find in Seconds. Explore the Best Info Now.
+    Search Preschool Sydney, Top Information From Trusted Internet Sources.  <br />
+    reschool Sydney, Get Expert Advice and Curated Content on Top10Quest.com. 100+ <br />
+    Unique Results. Useful & Relevant. Find All Info You Want. Quick Answers. Find in Seconds. Explore the Best Info Now.
     </p>
+
     </div>
   );
 }
