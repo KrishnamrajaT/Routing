198b2aee576e702416d260ab968ccd710786ce54
venkat vedicharlaa <venkat.vedicharla@gmail.com>
2022-07-12 17:00:53 +0530

    commit test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
+      <h1>Home page</h1>
     </div>
   );
 }
