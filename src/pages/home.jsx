f6973bc951788789423082416c1f094585c2991e
venkat vedicharlaa <venkat.vedicharla@gmail.com>
2022-07-12 18:32:13 +0530

    para added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <button>Home page</button>
+    <p>
+    Search Preschool Sydney, Top Information From Trusted Internet Sources. Preschool Sydney, Get Expert Advice and Curated Content on Top10Quest.com. 100+ Unique Results. Useful & Relevant. Find All Info You Want. Quick Answers. Find in Seconds. Explore the Best Info Now.
+    </p>
     </div>
   );
 }
