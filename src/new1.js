ce2b1aaa6be4162404efede60ec9236c526ecde7
venkat vedicharlaa <venkat.vedicharla@gmail.com>
2022-07-13 11:58:12 +0530

    new file added

---
+++ src/new1.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+export default function New() {
+  return (
+    <div>
+      <button>Home page</button>
+    <p>
+        test file
+    </p>
+
+        
+
+    </div>
+  );
+}
